34a43d478016d545ef32186549279f62302fa895
dhruvinsanghvi <dhruvinsanghvi@gmail.com>
2016-06-28 11:57:51 +0530

    local changes

--- js/main.js
+++ js/main.js
@@ -8,14 +8,13 @@ var x = $(".jumbotron").height();
     console.log("if");
     $(".navbar-default").css("background-color", "rgba(255,255,255,1)");
     $(".navbar-default .navbar-nav > li > a").css("color", "#000000");
-    $(".nav-tabs").css({"top": 50, "position":"fixed", "z-index":1000, "background-color":"#fff","left":"50%",
-      "transform":"translate(-50%, -50%)","top":"80px"});
+    //$(".nav-tabs").css({ "position":"fixed", "z-index":1000, "background-color":"#fff","top":"80px"});
  
   } else {
     console.log("else");
     $(".navbar-default").css("background-color", "transparent");
     $(".navbar-default .navbar-nav > li > a").css("color", "#ffffff");
-    $(".nav-tabs").css({"top": 0, "position":"static", "z-index":0, "background-color":"#fff","left":"0","top":"0px","transform":"translate(0%, -0%)"});
+    //$(".nav-tabs").css({"top": 0, "position":"static", "z-index":0, "background-color":"#fff","left":"0","top":"0px"});
 
  
 
@@ -29,5 +28,8 @@ var x = $(".jumbotron").height();
          $(this).children().stop().animate({height: 'toggle'});
        
     });
+
+    $('#navtab').affix({
+});
  
 });
