d8d9b0c45803ef2fca3c3d3d035edc7f0947d374
dhruvinsanghvi <dhruvinsanghvi@gmail.com>
2016-06-29 12:04:34 +0530

    bugs and content have been added and most of the bugs are resolved

--- js/main.js
+++ js/main.js
@@ -10,30 +10,14 @@
           $('.logo').addClass('color');
           $('.navbar-custom').css('background-color', 'rgba(255,255,255,1)');
           $('.navbar-custom .navbar-nav > li > a').css('color', '#000000');
-          $('.nav-tabs').css({
-              //'top': 50, [Abbas]: Why duplicate??
-              //'position': 'fixed', Never mess around with this
-              'z-index': 1000,
-              'background-color': '#fff',
-              'left': '50%',
-              'transform': 'translate(-50%, -50%)',
-              'top': '80px'
-          });
+         
 
       } else {
           console.log('else');
           $('.logo').removeClass('color');
           $('.navbar-custom').css('background-color', 'transparent');
           $('.navbar-custom .navbar-nav > li > a').css('color', '#ffffff');
-          $('.nav-tabs').css({
-              //'top': 0, Removing duplicate top value
-              'position': 'static',
-              'z-index': 0,
-              'background-color': '#fff',
-              'left': '0',
-              'top': '0px',
-              'transform': 'translate(0%, -0%)'
-          });
+         
       }
   });
   $(document).ready(function() {
@@ -46,9 +30,26 @@
       });
 
       /* Add all JS logic in one single file to minimise loading time and script dependency on every page */
-      
-      var scene = $('.scene');
+      var scene = document.getElementById('scene');
       var parallax = new Parallax(scene);
+
+     var z = $('.jumbotron').outerHeight();
+     $('.nav-tabs').affix({
+     offset: {
+     top: z,
+    
+      }
+    })
+   
+            var sideslider = $('[data-toggle=collapse-side]');
+            var sel = sideslider.attr('data-target');
+           
+            sideslider.click(function(event){
+                $(sel).toggleClass('in');
+          
+            });
+     
+    
 
   });
 
