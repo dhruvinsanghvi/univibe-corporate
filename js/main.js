04e946fb9e6b345938133678151de14c19cac567
dhruvinsanghvi <dhruvinsanghvi@gmail.com>
2016-06-29 17:49:01 +0530

    all bugs andcontent resolved

--- js/main.js
+++ js/main.js
@@ -10,6 +10,8 @@
           $('.logo').addClass('color');
           $('.navbar-custom').css('background-color', 'rgba(255,255,255,1)');
           $('.navbar-custom .navbar-nav > li > a').css('color', '#000000');
+          $('.theme-btn').css({'color':'#000000', 'border': '1px solid #000000','border-radius':'4px'});
+          $('.nav-tabs').addClass('navbar-fixed-top');
          
 
       } else {
@@ -17,6 +19,8 @@
           $('.logo').removeClass('color');
           $('.navbar-custom').css('background-color', 'transparent');
           $('.navbar-custom .navbar-nav > li > a').css('color', '#ffffff');
+          $('.nav-tabs').removeClass('navbar-fixed-top');
+           $('.theme-btn').css('color', '#ffffff');
          
       }
   });
@@ -25,29 +29,35 @@
 
       $('.team-img').hover(function() {
 
-          $(this).children().stop().animate({ height: 'toggle' });
+          $(this).children().fadeToggle( 500, "linear" );
 
       });
 
       /* Add all JS logic in one single file to minimise loading time and script dependency on every page */
-      var scene = document.getElementById('scene');
-      var parallax = new Parallax(scene);
+      //var scene = document.getElementById('scene');
+      //var parallax = new Parallax(scene);
 
-     var z = $('.jumbotron').outerHeight();
-     $('.nav-tabs').affix({
-     offset: {
-     top: z,
-    
+      var z = $('.jumbotron').outerHeight();
+      $('.nav-tabs').affix({
+      offset: {
+      top: z,
       }
-    })
+      });
    
-            var sideslider = $('[data-toggle=collapse-side]');
-            var sel = sideslider.attr('data-target');
-           
-            sideslider.click(function(event){
-                $(sel).toggleClass('in');
+      var sideslider = $('[data-toggle=collapse-side]');
+      var sel = sideslider.attr('data-target');
+      sideslider.click(function(event){
+      $(sel).toggleClass('in');
           
-            });
+      });
+      var offset = 130;
+
+    $('.nav-tabs li a').click(function(event) {
+    event.preventDefault();
+    $($(this).attr('href'))[0].scrollIntoView();
+    scrollBy(0, -offset);
+    });
+      
      
     
 
